d5f1149eaefb56305d4f0492c550d583dd43b101
lisixianCoder <357047750@qq,com>
2019-08-24 20:29:15 +0800

    msg

--- webhook.js
+++ webhook.js
@@ -1,44 +1,46 @@
 let http = require('http');
 let crypto = require('crypto');
-let {spawn} = require('child_process') //为了不阻塞档期那的进程，要开个子进程
+let {spawn} = require('child_process'); //为了不阻塞档期那的进程，要开个子进程
 let SECRET = '123456'
 function sign(body) {
     return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex')
 }
 let server = http.createServer(function(req,res){
-    if(req.method=='POST' && req.ur=="/webhook"){
+    console.log(req.method,req.url);
+    if(req.method == 'POST' && req.url == '/webhook'){
         let buffers = [];
         req.on('data',function(buffer){
-            buffers.push(buffer)
-        })
+            buffers.push(buffer);
+        });
         req.on('end',function(buffer){
-            let body = Buffer.concat(buffers)
-            let event = req.headers['x-gitHub-event'];
-            let signature = req.headers['x-hub-signature']
-            if(signature === sign(body)){
-                return res.end('NOT ALLOW')
+            let body = Buffer.concat(buffers);
+            let event = req.headers['x-github-event'];//event=push
+            //github请求来的时候，要传递请求体body,另外还会传一个signature过来，你需要验证签名对不对
+            let signature = req.headers['x-hub-signature'];
+            if(signature  !== sign(body)){
+              return res.end('Not Allowed');
             }
-            res.setHeader('Content-Type','application/json')
-            res.end(JSON.stringify({ok:true}))
-            if(event=='push'){
-                let payload = JSON.parse(body);
-                //返回一个子进程
-                let child = spawn('sh',[`./${payload.repository.name}.sh`]);
-                let buffers = [];
-                child.stdout.on('data',function(buffer){
-                    buffers.push(buffer)
-                })
-                child.stdout.on('end',function(buffer){
-                    let log = Buffer.concat(buffers)
-                    console.log(log)
-                })
+            res.setHeader('Content-Type','application/json');
+            res.end(JSON.stringify({ok:true}));
+            if(event == 'push'){//开始布署
+                 let payload = JSON.parse(body);
+                 //返回一个子进程
+                 let child = spawn('sh',[`./${payload.repository.name}.sh`]);
+                 let buffers = [];
+                 child.stdout.on('data',function(buffer){
+                     buffers.push(buffer);
+                 });
+                  child.stdout.on('end',function(buffer){
+                     let logs = Buffer.concat(buffers).toString();
+                     console.log(log)
+                 });
             }
-
-        })
+        });
+       
     }else{
-        res.end('Not Found')
+        res.end('Not Found');
     }
-})
+ });
 server.listen(4000,function(){
     console.log('webhook 在4000端口启动....')
 })
