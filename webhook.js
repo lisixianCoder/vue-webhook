9c7bd64f524518faa09e724ded841f70e019c586
lisixianCoder <357047750@qq,com>
2019-08-24 18:15:00 +0800

    msg

--- webhook.js
+++ webhook.js
@@ -1,8 +1,23 @@
 let http = require('http');
+let crypto = require('crypto');
+let SECRET = '123456'
+function sign(body) {
+    return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex')
+}
 let server = http.createServer(function(req,res){
     if(req.method=='POST' && req.ur=="/webhook"){
-        res.setHeader('Content-Type','application/json');
-        res.end(JSON.stringify({ok:true}))
+        let buffers = [];
+        req.on('data',function(buffer){
+            buffers.push(buffer)
+        })
+        req.on('end',function(buffer){
+            let body = Buffer.concat(buffers)
+            let event = req.header['x-gitHub-event'];
+            let signature = req.headers['x-hub-signature']
+            if(signature === sign(body)){
+                res.end('NOT ALLOW')
+            }
+        })
     }else{
         res.end('Not Found')
     }
