89155fb71b6cd9bf2bcc6f99e7367e40a5991677
lisixianCoder <357047750@qq,com>
2019-08-24 16:40:32 +0800

    init

---
+++ webhook.js
@@ -0,0 +1,12 @@
+const http = require('http');
+let server = http.createServer((req,res)=>{
+    if(req.method=='POST' && req.ur=="/webhook"){
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify({ok:true}))
+    }else{
+        res.end('Not Found')
+    }
+})
+server.listen(4000,()=>{
+    console.log('webhook 在4000端口启动')
+})
