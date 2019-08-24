b86a8cbc0b24e8eaf78801c9c3cd0a2380349e7e
lisixianCoder <357047750@qq,com>
2019-08-24 20:12:01 +0800

    msg

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 let http = require('http');
 let crypto = require('crypto');
+let {spawn} = require('child_process') //为了不阻塞档期那的进程，要开个子进程
 let SECRET = '123456'
 function sign(body) {
     return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex')
@@ -16,9 +17,23 @@ let server = http.createServer(function(req,res){
             let signature = req.headers['x-hub-signature']
             if(signature === sign(body)){
                 return res.end('NOT ALLOW')
-            }else {
-                
             }
+            res.setHeader('Content-Type','application/json')
+            res.end(JSON.stringify({ok:true}))
+            if(event=='push'){
+                let payload = JSON.parse(body);
+                //返回一个子进程
+                let child = spawn('sh',[`./${payload.repository.name}.sh`]);
+                let buffers = [];
+                child.stdout.on('data',function(buffer){
+                    buffers.push(buffer)
+                })
+                child.stdout.on('end',function(buffer){
+                    let log = Buffer.concat(buffers)
+                    console.log(log)
+                })
+            }
+
         })
     }else{
         res.end('Not Found')
